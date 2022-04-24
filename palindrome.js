d14f5cd90195f15bdc6cad1d46298a4caea0b1b8
Asvindra Rajpoot <aswin1399904@gmail.com>
2022-04-24 23:15:25 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,14 @@
 //Check string is palindrom
+const revStr = (str) => {
+  // return str.split('').reverse().join('');
+  let res = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    res = char + res;
+  }
+  return res;
+};
+
 function isPalindrom(str) {
   const res = revStr(str);
   if (res === str) {
